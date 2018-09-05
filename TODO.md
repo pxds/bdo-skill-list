95f5f7ab3b87b1a60383c2f96be6fdd9ad0aeb5a
Victor Praxedes <victor.praxedes@gmail.com>
2018-09-04 21:45:16 -0300

    getting skill names and link with the skill id
    added a TODO list

---
+++ TODO.md
@@ -0,0 +1,8 @@
+#TO DO LIST
+
+* Get rid of skills with the same name i.e. Hastiludium I II III
+* Implement ```bdo_class``` to hold information of all class' skills
+* Use the skill url from ```print_skill_info``` to get skill details
+* Compare the details against the ```skill_modifier``` object and start classifying the modifier component on the ```bdo_class```
+* ??????
+* Profit
--- html-scraper.py
+++ html-scraper.py
@@ -2,14 +2,34 @@ from bs4 import BeautifulSoup
 import requests
 import json
 
-url = "https://www.invenglobal.com/blackdesertonline/skill/layer/"
-skill_id = 1952
+def print_skill_info(soup):
+	for tr in skill_list.find_all('tr'):
+		td_skillname = tr.find('td',{'class':'skillname left'})
+		if td_skillname:
+			print(td_skillname.find('span').string,
+				# for some reason it prints // with one character
+				('http:/'+td_skillname.a.get('href')))
+			pass
 
-response = requests.get(url+str(skill_id))
+class_id = {"Warrior":"1", "Ranger":"5", "Sorceress":"9", "Berserker":"13", "Tamer":"17", 
+			"Musa":"21", "Valkyrie":"25", "Maewha":"22", "Wizard":"29", "Witch":"32",
+			"Witch":"32", "Kunoichi":"26", "Ninja":"27", "Dark Knight":" 28", "Striker":"20", "Mystic": "24"}
 
-json_data = json.loads(response.text)
-html_data = json_data['data']
+skill_modifiers = {
+	"cc" : ["Stiffness", "Stun", "Knockdown", 
+	        "Bound", "Knockback", "Floating", 
+	        "Grapple", "Freezing"],
+	"armor" : ["Forward Guard", "Super Armor", "Invincible"]
+}
+# first_skill_id = 1
+# last_skill_id = 2773
 
-soup = BeautifulSoup(html_data)
+# populate class skill list
+url = "https://www.invenglobal.com/blackdesertonline/skill/"
 
-print(soup.get_text())
+response = requests.get(url+class_id["Valkyrie"])
+soup = BeautifulSoup(response.text, features="html.parser")
+
+skill_list = soup.main.find("div", {"class":"content skillList"})
+
+print_skill_info(skill_list)
